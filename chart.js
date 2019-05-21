7ebd85120566ddcf35a9f2c90cc66f3b949d558a
김성현 <iampet1@naver.com>
2019-05-21 19:00:40 +0900

    항목 커서이벤트 완료

--- chart.js
+++ chart.js
@@ -17,7 +17,7 @@ var ctx = canvas.getContext("2d");
  * 
  * @type {Number}
  */
-var radius = canvas.height / 2;
+var radius = null;
 
 /**
  * 항목 목록
@@ -25,11 +25,11 @@ var radius = canvas.height / 2;
  * @type {Array}
  */
 var items = [
-    { "nm": "sh1", "high": 80 },
-    { "nm": "sh2", "high": 50 },
-    { "nm": "sh3", "high": 70 },
-    { "nm": "sh4", "high": 50 },
-    { "nm": "sh5", "high": 80 },
+    { "nm": "sh1", "high": 80, "position": null },
+    { "nm": "sh2", "high": 50, "position": null },
+    { "nm": "sh3", "high": 70, "position": null },
+    { "nm": "sh4", "high": 50, "position": null },
+    { "nm": "sh5", "high": 80, "position": null },
 ];
 
 /**
@@ -40,9 +40,20 @@ var items = [
 var isMouseDown = false;
 
 /**
+ * 
+ * 
+ * @type {Boolean}
+ */
+var rects = [];
+
+var chartHelper;
+
+/**
  * 차트 최초호출 함수다.
  */
 (function init() {
+    chartHelper = chartHelperFn();
+    radius = canvas.height / 2;
     ctx.translate(radius, radius);
     radius = radius * 0.80;
     drawChart();
@@ -74,21 +85,27 @@ function drawCircle() {
  * 항목을 그린다.
  */
 function drawItem() {
-    var ang;
-    var idx;
+    var ang = 0,
+        idx = 0,
+        rect = {};
+    rects = [];
+
     ctx.font = radius * 0.11 + "px arial";
     ctx.textBaseline = "middle";
     ctx.textAlign = "center";
-    for (idx = 0; idx < items.length; idx++) {
+    for (idx; idx < items.length; idx++) {
         ang = idx * Math.PI / (items.length / 2);
+        x = Math.sin(ang) * radius * 1.15;
+        y = -Math.cos(ang) * radius * 1.1;
+        rect = { "x": x - 20, "y": y - 20, "w": ctx.measureText(items[idx].nm).width + 4, "h": 40 };
 
-        ctx.rotate(ang);
-        ctx.translate(0, -radius * 1.15);
-        ctx.rotate(-ang);
-        ctx.fillText(items[idx].nm, 0, 0);
-        ctx.rotate(ang);
-        ctx.translate(0, radius * 1.15);
-        ctx.rotate(-ang);
+        ctx.lineWidth = 4;
+        ctx.strokeStyle = "#E3E3E3";
+        ctx.fillStyle = "white";
+        chartHelper.roundRect(ctx, rect.x, rect.y, rect.w, rect.h, 10, true);
+        ctx.fillStyle = "#000000";
+        ctx.fillText(items[idx].nm, x, y);
+        rects.push(rect);
     }
 }
 
@@ -97,18 +114,23 @@ function drawItem() {
  */
 function drawDirection() {
     var ang;
+
     ctx.beginPath();
     ctx.strokeStyle = "#E3E3E3";
     for (var idx = 0; idx < items.length; idx++) {
         ang = idx * Math.PI / (items.length / 2);
-        ctx.lineWidth = radius * 0.01;
-        ctx.lineCap = "round";
+
+        x = Math.sin(ang) * radius;
+        y = -Math.cos(ang) * radius;
         ctx.moveTo(0, 0);
-        ctx.rotate(ang);
-        ctx.lineTo(0, -radius);
+        ctx.lineTo(x, y);
+        ctx.lineWidth = 6;
         ctx.stroke();
-        ctx.rotate(-ang);
     }
+
+}
+function cloneObject(obj) {
+    return Object.assign({}, obj);
 }
 
 /**
@@ -140,43 +162,105 @@ function drawRange() {
 
 /**
  * 마우스 다운
+ * 
  * @param {MouseEvent} 마우스 이벤트 객체
  */
 canvas.onmousedown = function (e) {
     isMouseDown = true;
-    console.log(e.target)
 }
 
 /**
  * 마우스 무브
+ * 
  * @param {MouseEvent} 마우스 이벤트 객체
  */
 canvas.onmousemove = function (e) {
+    // TODO 좌표 표시를 위한 임시로직
+    var c = chartHelper.getMousePos(e);
+    document.querySelector('p')
+        .innerHTML = `X = ${c.x} Y = ${c.y}`;
 
-
-    document.getElementById('x')
-        .innerHTML = "X:::::" + (e.clientX - ctx.canvas.offsetLeft - canvas.width / 2) + "Y::::" + (e.clientY - ctx.canvas.offsetTop - canvas.height / 2);
+    // 
+    chartHelper.checkCursorRange(e);
 }
 
 /**
  * 마우스 업
+ * 
  * @param {MouseEvent} 마우스 이벤트 객체
  */
 canvas.onmouseup = function (e) {
     isMouseDown = false;
 }
 
-
-
-
-
-
-
-
 /**
  * 마우스 리브
+ * 
  * @param {MouseEvent} 마우스 이벤트 객체
  */
 canvas.onmouseleave = function (e) {
     isMouseDown = false;
 }
+
+/** ====================================================================================================================================*
+ *                                                         public                                                                       *  
+ *======================================================================================================================================*/
+
+/**
+ * 차트 헬퍼 함수이다.
+ */
+function chartHelperFn() {
+    return {
+        roundRect: function (ctx, x, y, width, height, radius, fill, stroke) {
+            if (typeof stroke == "undefined") {
+                stroke = true;
+            }
+            if (typeof radius === "undefined") {
+                radius = 5;
+            }
+            ctx.beginPath();
+            ctx.moveTo(x + radius, y);
+            ctx.lineTo(x + width - radius, y);
+            ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+            ctx.lineTo(x + width, y + height - radius);
+            ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+            ctx.lineTo(x + radius, y + height);
+            ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+            ctx.lineTo(x, y + radius);
+            ctx.quadraticCurveTo(x, y, x + radius, y);
+            ctx.closePath();
+            if (stroke) {
+                ctx.stroke();
+            }
+            if (fill) {
+                ctx.fill();
+            }
+        },
+
+        //TODO 클릭이벤트로 접근 시 선택된 항목이 무엇인지 캐싱해놓기
+        checkCursorRange: function (e) {
+            var p = this.getMousePos(e);
+            rects.some(rect => {
+                if (p.x >= rect.x && p.x <= rect.x + rect.w &&
+                    p.y >= rect.y && p.y <= rect.y + rect.h) {
+                    canvas.style.cursor = "pointer";
+                    return true;
+                }
+
+                canvas.style.removeProperty("cursor");
+                return false;
+            });
+        },
+
+        getMousePos: function (e) {
+            var r = canvas.getBoundingClientRect();
+            return {
+                x: e.clientX - r.left - canvas.width / 2,
+                y: e.clientY - r.top - canvas.height / 2
+            };
+        }
+    }
+}
+
+
+
